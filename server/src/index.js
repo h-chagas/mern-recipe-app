66c4eea1d93691f8afcf0f894d985b2f3dc356d4
Hugo Chagas <hugochagasuk@gmail.com>
2023-06-09 08:53:40 +0100

    initial server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,12 @@
 // Setup Express
 import express from "express"
+import cors from "cors"
+import mongoose from 'mongoose'
+
+const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+app.listen(3001, () => console.log("Server started!"));
