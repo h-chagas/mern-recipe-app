b3713b6c8dafaab4f2962bbb8853903db53ee90e
Hugo Chagas <hugochagasuk@gmail.com>
2023-06-16 12:30:08 +0100

    import and use recipesRouter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import express from "express"
 import cors from "cors"
 import mongoose from 'mongoose'
 import { userRouter } from './routes/users.js'
+import { recipesRouter } from './routes/recipes.js'
 
 dotenv.config()
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 //db connection
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@recipes.jehmlkb.mongodb.net/recipes?retryWrites=true&w=majority`)
